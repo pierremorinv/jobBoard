c7f741d6beae9497d12938dddff2572a6a6fa5b9
Pierre Morin <144466190+pierremorinvo@users.noreply.github.com>
2023-10-21 15:30:34 +0200

    add admin method login

--- front-end/javascript/login.js
+++ front-end/javascript/login.js
@@ -19,10 +19,18 @@ fetch("http://localhost:3000/api/peoples")
       const matchUser = users.find(
         (usercreate) =>
           usercreate.email === user.email &&
-          usercreate.password === user.password
+          usercreate.password === user.password &&
+          usercreate.admin === false
       );
-
-      if (matchUser) {
+      const matchAdminUser = users.find(
+        (usercreate) =>
+          usercreate.email === user.email &&
+          usercreate.password === user.password &&
+          usercreate.admin == true
+      );
+      if (matchAdminUser) {
+        window.open("http://127.0.0.1:5500/front-end/html/admin.html");
+      } else if (matchUser) {
         window.open("http://127.0.0.1:5500/front-end/index.html");
       } else {
         const matchEmail = users.find(
